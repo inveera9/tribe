9ce4004e84b8806f67694b2777ffb3bb3f0a8f1c
Kevin Kantz <kkantz@myyouq.com>
2015-08-01 13:41:30 -0700

    removed and reset DB and some migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710163104) do
+ActiveRecord::Schema.define(version: 20150709213928) do
 
   create_table "attachinary_files", force: true do |t|
     t.integer  "attachinariable_id"
@@ -76,10 +76,6 @@ ActiveRecord::Schema.define(version: 20150710163104) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
   end
 
   add_index "pins", ["user_id"], name: "index_pins_on_user_id"
@@ -126,10 +122,6 @@ ActiveRecord::Schema.define(version: 20150710163104) do
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.integer  "invitations_count",      default: 0
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
