c09988e396a11adf45d1661d7e0b2dc2455b59fb
Prashant Vardhan <prashant.vardhan@inveera.us>
2015-08-12 19:09:51 +0530

    Update memorabilia

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -2,7 +2,5 @@ class Memory < ActiveRecord::Base
     belongs_to :user
     has_many :memorables, dependent: :destroy
 	validates :title, presence: true
-	validates :description, presence: true
 
-	acts_as_taggable
 end
